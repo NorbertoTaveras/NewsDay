412fefb8815c875f809de48ea2a53ecf90ba46ff
Norberto Taveras <nm.taveras@gmail.com>
2019-09-17 13:03:37 -0400

    ADD - The user interface helper/utilities class, which will allow to modal dialogs and confirmation when needed.

---
+++ NewsDay/NewsDay/Utilities/UIUtils.swift
@@ -0,0 +1,48 @@
+//
+//  UIUtils.swift
+//  NewsDay
+//
+//  Created by Norberto Taveras on 9/17/19.
+//  Copyright © 2019 Norberto Taveras. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UIUtils {
+    
+    static func modalDialog(parent: UIViewController, title: String, message: String) {
+        let alert = UIAlertController.init(
+            title: title,
+            message: message, preferredStyle: .alert)
+        
+        let okButton = UIAlertAction.init(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(okButton)
+        
+        parent.present(alert, animated: true, completion: nil)
+    }
+    
+    static func modalConfirm(parent: UIViewController,
+                             title: String, message: String,
+                             completionHandler: @escaping (Bool) -> Void) {
+        let alert = UIAlertController.init(
+            title: title,
+            message: message, preferredStyle: .alert)
+        
+        let okButton = UIAlertAction.init(
+            title: "OK", style: .default, handler: { _ in
+                completionHandler(true)
+        })
+        
+        let cancelButton = UIAlertAction.init(
+            title: "Cancel", style: .default, handler: { _ in
+                completionHandler(false)
+        })
+        
+        alert.addAction(okButton)
+        alert.addAction(cancelButton)
+        
+        parent.present(alert, animated: true, completion: nil)
+    }
+}
