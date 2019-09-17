bc595349b6a9297a6d68bb63f9da36e11e6586f5
Norberto Taveras <nm.taveras@gmail.com>
2019-09-17 09:15:21 -0400

    ADD - NewsDay initial Xcode project files and solution to begin development.

---
+++ NewsDay/NewsDay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsDay
+//
+//  Created by Norberto Taveras on 9/17/19.
+//  Copyright © 2019 Norberto Taveras. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
