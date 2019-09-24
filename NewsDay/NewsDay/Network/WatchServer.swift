01cb0833b3f920fbe99b3df37a4ab1b58fd014e1
Norberto Taveras <nm.taveras@gmail.com>
2019-09-24 18:15:31 -0400

    Articles/Headlines/Image Received Message Handling & Resize Image
    ADD - cases scenarios when receiving messages for the articles, headlines and image of article in the watch companion application.
    ADD - method to resize article image accordingly through specific watch dimensions.

--- NewsDay/NewsDay/Network/WatchServer.swift
+++ NewsDay/NewsDay/Network/WatchServer.swift
@@ -9,6 +9,7 @@
 import Foundation
 import WatchKit
 import WatchConnectivity
+import Kingfisher
 
 class WatchServer: NSObject, WCSessionDelegate {
     
@@ -56,10 +57,113 @@ class WatchServer: NSObject, WCSessionDelegate {
                     }
                 })
                 
+            case "articles":
+                let sourceData = message["source"] as? Data
+                let optionalSource = Source.keyedUnarchive(data: sourceData)
+                
+                guard let source = optionalSource else { return }
+                
+                NewsApi.getArticles(source: source).withResult { (result, error) in
+                    guard let articles = result as? [Article] else { return }
+                    
+                    if let data = Article.keyedArchivedArray(articles: articles) {
+                        let reply: [String: Any] = [
+                            "articles": data
+                        ]
+                        
+                        replyHandler(reply)
+                    }
+                }
+                
+            case "headlines":
+                guard let country = message["country"] as? String else { return }
+                guard let limit = message["limit"] as? Int else { return }
+                
+                NewsApi.getHeadlines(country: country, limit: limit)
+                    .withResult { (result, error) in
+                        guard let headlines = result as? [Article] else { return }
+                        
+                        if let data = Article.keyedArchivedArray(articles: headlines) {
+                            let reply: [String: Any] = [
+                                "articles": data
+                            ]
+                            
+                            replyHandler(reply)
+                        }
+                }
+                
+            case "image":
+                guard let urlText = message["url"] as? String else { return }
+                guard let url = URL(string: urlText) else { return }
+                
+                KingfisherManager.shared.retrieveImage(with: url) { result in
+                    guard let image = try? result.get().image else { return }
+                    
+                    // Resize the image down to something reasonabe for a watch
+                    // and greatly reduce bluetooth transfer size
+                    guard let resizedImage = WatchServer.resizeImage(
+                        image: image,
+                        maxSize: CGSize(width: 448, height: 368))
+                        else { return }
+                    
+                    let imageData = resizedImage.jpegData(compressionQuality: 0.0)
+                    
+                    let reply: [String: Any] = [
+                        "image": imageData as Any
+                    ]
+                    
+                    replyHandler(reply)
+                }
+                
             default:
                 replyHandler(["error":"Unrecognized request"])
             }
         }
     }
     
+    private static func resizeImage(image: UIImage, maxSize: CGSize) -> UIImage? {
+        let hasAlpha = false
+        let scale: CGFloat = 0.0 // Automatically use scale factor of main screen
+        
+        // Initially output size is the same as input size
+        var outputSize = image.size
+        
+        // Skip unnecessary resize
+        if outputSize.width <= maxSize.width &&
+            outputSize.height <= maxSize.height {
+            // Just return the original image
+            return image
+        }
+        
+        // Make it proportially narrower if output is too wide
+        if outputSize.width > maxSize.width {
+            let scale = maxSize.width / outputSize.width
+            
+            // Scale width and height the same amount to preserve aspect
+            outputSize.width *= scale
+            outputSize.height *= scale
+        }
+        
+        // Make it proportionally shorter if output is too tall
+        if outputSize.height > maxSize.height {
+            let scale = maxSize.height / outputSize.height
+            
+            // Scale width and height same amount to preserve aspect
+            outputSize.width *= scale
+            outputSize.height *= scale
+        }
+        
+        UIGraphicsBeginImageContextWithOptions(outputSize, !hasAlpha, 1.0)
+        
+        // Guarantee that the graphics context is ended, even on error/exception
+        defer {
+            UIGraphicsEndImageContext()
+        }
+        
+        let outputRect = CGRect(origin: CGPoint.zero, size: outputSize)
+        image.draw(in: outputRect)
+        
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        return scaledImage
+    }
 }
