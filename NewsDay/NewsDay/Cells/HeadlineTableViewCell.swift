eef13af7c7ade0f879f5195425f755a1e83c4d26
Norberto Taveras <nm.taveras@gmail.com>
2019-09-26 08:10:02 -0400

    Headline View Controller
    ADD - headline view controllers which will download all the possible headlines through an async request and processing the headlines as well to be displayed later on.

---
+++ NewsDay/NewsDay/Cells/HeadlineTableViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  HeadlineTableViewCell.swift
+//  NewsDay
+//
+//  Created by Norberto Taveras on 9/26/19.
+//  Copyright © 2019 Norberto Taveras. All rights reserved.
+//
+
+import UIKit
+import Kingfisher
+
+class HeadlineTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var headlineImage: UIImageView!
+    @IBOutlet weak var headlineTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+    func setupHeadline(title: String, imageUrl: URL?) {
+        headlineTitle.text = title
+        if imageUrl != nil {
+            headlineImage.kf.indicatorType = .activity
+            headlineImage.kf.setImage(with: imageUrl)
+        } else {
+            let unavailableImage = UIImage(named: "news_default.png")
+            headlineImage.image = unavailableImage
+        }
+    }
+
+}
---
+++ NewsDay/NewsDay/Controllers/HeadlineViewController.swift
@@ -0,0 +1,39 @@
+//
+//  HeadlineViewController.swift
+//  NewsDay
+//
+//  Created by Norberto Taveras on 9/26/19.
+//  Copyright © 2019 Norberto Taveras. All rights reserved.
+//
+
+import UIKit
+
+class HeadlineViewController: ArticleViewBase {
+
+    @IBOutlet weak var headlineTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        initTable(table: headlineTableView)
+        
+        let resultWrapper = AsyncRequest()
+        NewsApi.downloadHeadlines(
+            country: "us",
+            limit: nil,
+            resultWrapper: resultWrapper)
+        
+        processArticles(resultWrapper: resultWrapper)
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        title = "Headlines"
+        tabBarController?.tabBar.isHidden = false
+    }
+    
+    @IBAction func back(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
