d02c8650b0c784f641d95cb916f00be0e02807d0
Norberto Taveras <nm.taveras@gmail.com>
2019-09-26 08:08:46 -0400

    Article Table View Cell
    ADD - a table view cell to represent a generic article to be displayed in the user interface.

---
+++ NewsDay/NewsDay/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  ArticleTableViewCell.swift
+//  NewsDay
+//
+//  Created by Norberto Taveras on 9/26/19.
+//  Copyright © 2019 Norberto Taveras. All rights reserved.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var articleImage: UIImageView!
+    @IBOutlet weak var articleTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    func setupArticleCell(imageUrl: URL?, title: String) {
+        if imageUrl != nil {
+            articleImage.kf.indicatorType = .activity
+            articleImage.kf.setImage(with: imageUrl)
+        } else {
+            let unavailableImage = UIImage(named: "news_default.png")
+            articleImage.image = unavailableImage
+        }
+        articleTitle.text = title
+    }
+}
